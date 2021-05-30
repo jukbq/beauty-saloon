226874597dd5e61f204378e9d564367efbe1eccb
Yurii Ohlii <jukbq77@gmail.com>
2021-05-30 12:56:36 +0300

    30052021
    Меню
    Слайдер
    Соцсети

--- header.php
+++ header.php
@@ -25,7 +25,7 @@
         <div class="wraper">
             <?php the_custom_logo( 0 ); ?>
 
-         <div></div>
+        
 
             <nav class="wrap_menu" >
 
@@ -53,3 +53,50 @@
         </div>
 
     </header>
+<!----------------Social network-------------------->
+    <section class="social_networks">
+
+        <a class="fb" href="<?php echo get_theme_mod('facebook'); ?>" target="_blank">
+            <div class="sn_box facebook">
+                <p class="open_sn_bpx"><?php echo get_theme_mod('facebook'); ?></p>
+                <i class="fab fa-facebook-f"></i>
+            </div>
+        </a>
+        <a class="insta" href="<?php echo get_theme_mod('instagrann'); ?>" target="_blank">
+            <div class="sn_box instagrann">
+                <p class="open_sn_bpx"><?php echo get_theme_mod('instagrann'); ?></p>
+                <i class="fab fa-instagram"></i>
+            </div>
+        </a>
+        <a class="mas" href="https://www.messenger.com/t/<?php echo get_theme_mod('messenger'); ?>" target="_blank">
+            <div class="sn_box messenger">
+                <p class="open_sn_bpx"><?php echo get_theme_mod('messenger'); ?></p>
+                <i class="fab fa-facebook-messenger"></i>
+            </div>
+        </a>
+        <a class="vib" href="viber://chat?number=+<?php echo get_theme_mod('viber'); ?>">
+            <div class="sn_box viber">
+                <p class="open_sn_bpx"><?php echo get_theme_mod('viber'); ?></p>
+                <i class="fab fa-viber"></i>
+            </div>
+        </a>
+
+        <a class="tel_g" href="tg://resolve?domain=<?php echo get_theme_mod('telegram'); ?>">
+            <div class="sn_box telegram">
+                <p class="open_sn_bpx"><?php echo get_theme_mod('telegram'); ?></p>
+                <i class="fab fa-telegram-plane"></i>
+            </div>
+        </a>
+        <a lass="wa" href="whatsapp://send?phone=+<?php echo get_theme_mod('whatsapp') ?>">
+            <div class="sn_box whatsapp">
+                <p class="open_sn_bpx"><?php echo get_theme_mod('whatsapp') ?></p>
+                <i class="fab fa-whatsapp"></i>
+            </div>
+        </a>
+        <a class="yt" href="<?php echo get_theme_mod('youtube') ?>" target=" _blank">
+            <div class="sn_box youtube">
+                <p class="open_sn_bpx"><?php echo get_theme_mod('youtube') ?></p>
+                <i class="fab fa-youtube"></i>
+            </div>
+        </a>
+    </section>
