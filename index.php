bb98bedf92de188a258e046b850442de1841cb76
Yurii Ohlii <jukbq77@gmail.com>
2021-06-21 18:11:44 +0300

    adaptuve aboutus

--- index.php
+++ index.php
@@ -21,13 +21,15 @@ echo do_shortcode('[smartslider3 slider="2"]');
 
         <div class="about_us_block">
             <h3 class="block_title au_title">
-                             <?php the_field('about_us_title') ?>
+                <?php the_field('about_us_title') ?>
             </h3>
             <div class="about_us_text_foto">
                 <div class="about_us_text">
                     <?php the_field('about_us_text') ?>
                 </div>
-                <img src="<?php the_field('about_foto') ?>">
+               <div class="about_foto">
+                    <img src="<?php the_field('about_foto') ?>">
+                </div>
             </div>
         </div>
 
@@ -35,15 +37,15 @@ echo do_shortcode('[smartslider3 slider="2"]');
     <!---------------services---------------->
 
     <section id="services" class="services">
-  <div class="body_img_left bottom">
+        <div class="body_img_left bottom">
             <img src="<?php bloginfo('template_url'); ?>/assets/img/top-left.png" alt="">
         </div>
         <div class="body_img_right top">
             <img src="<?php bloginfo('template_url'); ?>/assets/img/bottom-right.png" alt="">
         </div>
-         <h3 class="block_title">
-                             <?php the_field('service_title') ?>
-            </h3>
+        <h3 class="block_title">
+            <?php the_field('service_title') ?>
+        </h3>
 
         <div class="service_block">
             <?php
@@ -58,7 +60,7 @@ echo do_shortcode('[smartslider3 slider="2"]');
                     <a href="<?php the_field('serv_page')?>">
                         <div class="icon">
                             <div class="icon_service">
-                                <img src="<?php the_field('icon_service_img');?>" >
+                                <img src="<?php the_field('icon_service_img');?>">
                             </div>
                             <div class="frame_icon"></div>
                         </div>
@@ -81,17 +83,17 @@ echo do_shortcode('[smartslider3 slider="2"]');
 
     <section id="foto_galery" class="foto_galery">
 
-       <div class="body_img_left top">
+        <div class="body_img_left top">
             <img src="<?php bloginfo('template_url'); ?>/assets/img/top-left.png" alt="">
         </div>
         <div class="body_img_right bottom">
             <img src="<?php bloginfo('template_url'); ?>/assets/img/bottom-right.png" alt="">
         </div>
 
-         <h3 class="block_title">
-                             <?php the_field('foto_galery_title') ?>
-            </h3>
-      <div class="gallery_block">
+        <h3 class="block_title">
+            <?php the_field('foto_galery_title') ?>
+        </h3>
+        <div class="gallery_block">
             <?php
             $featured_posts = get_field('gallery_block');
             if ($featured_posts) :
@@ -104,7 +106,7 @@ echo do_shortcode('[smartslider3 slider="2"]');
                     <a href="<?php the_field('gallery_page')?>">
                         <div class="icon">
                             <div class="icon_gallery">
-                                <img src="<?php the_field('icon_gallery_img');?>" >
+                                <img src="<?php the_field('icon_gallery_img');?>">
                             </div>
                             <div class="frame_icon"></div>
                         </div>
@@ -121,19 +123,19 @@ echo do_shortcode('[smartslider3 slider="2"]');
             <?php endif; ?>
 
         </div>
-</div>
+        </div>
 
-       </div>
+        </div>
 
 
     </section>
 
     <!-----------------------brands--------------------->
     <section class="brends">
-         <div class="body_img_right top">
+        <div class="body_img_right top">
             <img src="<?php bloginfo('template_url'); ?>/assets/img/bottom-right.png" alt="">
         </div>
-    <?php echo do_shortcode('[slick_logo_carousel id="480"]'); ?>->
+        <?php echo do_shortcode('[slick_logo_carousel id="480"]'); ?>->
     </section>
 
 
